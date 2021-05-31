a3020a8291534e49d0be180b7caabf18b65904df
abbasebadian <abbasebadiann@gmail.com>
2021-05-31 22:12:25 +0430

    imported timeUtils module

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var http = require('http');
+var timeUtils = require('./TimeUtils.js')
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+  res.end(`Hello World! Current Time is: ${timeUtils.getCurrentTime()}` );
 }).listen(8080);
